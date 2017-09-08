6ce35fb1827431f021cfa45dfdc7534bb93f7621
David Klein <dave@smokinheads.de>
2017-09-08 18:31:27 +0200

    add EnumComboProperty.value field; closes #1

--- zui/props/EnumComboProperty.hx
+++ zui/props/EnumComboProperty.hx
@@ -5,19 +5,25 @@ import zui.Zui;
 class EnumComboProperty<E: EnumValue> {
 	var e: Enum<E>;
 	public var index(get, set): Int;
+	public var value(get, set): E;
 	public var handle(default, null) = new Handle();
 	public var items(get, never): Array<String>;
 	
-	public function new( e: Enum<E>) {
+	public function new( e: Enum<E> )
 		this.e = e;
-	}
 	
 	inline function get_index()
 		return handle.position;
 		
-	inline function set_index( i ) {
-		handle.position = i;
-		return i;
+	inline function set_index( i )
+		return handle.position = i;
+	
+	inline function get_value()
+		return Type.createEnumIndex(e, index);
+		
+	inline function set_value( v: E ) : E {
+		handle.position = Type.enumIndex(v);
+		return v;
 	}
 	
 	inline function get_items()
